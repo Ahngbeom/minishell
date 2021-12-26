d94f320ff8070895bfbf364e913944dba7eb8f74
ahngbeom <bbu0704@gmail.com>
2021-12-26 23:45:25 +0900

    Refactoring remove_enclosed_quotes

--- srcs/utils/remove_enclosed_quotes.c
+++ srcs/utils/remove_enclosed_quotes.c
@@ -6,40 +6,70 @@
 /*   By: bahn <bahn@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/23 14:30:34 by bahn              #+#    #+#             */
-/*   Updated: 2021/12/24 23:14:15 by bahn             ###   ########.fr       */
+/*   Updated: 2021/12/26 23:44:42 by bahn             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static	int	quotes_finder(char *argv, char quetes)
+static	int	quotes_finder(char **arg, char quetes)
 {
+	char	*origin;
 	char	*find;
+	char	*r_find;
+	char	*temp;
+	char	*temp2;
 
-	find = ft_strchr(argv, quetes);
+	find = ft_strchr(*arg, quetes);
 	if (find != NULL && *(find - sizeof(char)) != '\\')
 	{
-		if (find == ft_strrchr(argv, quetes))
+		r_find = ft_strrchr(*arg, quetes);
+		if (find == r_find)
 		{
 			return (1);
 		}
 		else
+		{
+			origin = ft_substr(*arg, 0, ft_strlen(*arg) - ft_strlen(find) );
+			temp = origin;
+			temp2 = ft_substr(find, 1, r_find - find - 1);
+			origin = ft_strjoin(origin, temp2);
+			free(temp);
+			free(temp2);
+			if (*(r_find + 1) != '\0')
+			{
+				*arg = ft_strjoin(origin, r_find + 1);
+				free(origin);
+			}
+			else
+			{
+				free(*arg);
+				*arg = origin;
+			}
 			return (0);
+		}
 	}
 	else
 		return (0);
 }
 
-char	*remove_enclosed_quotes(char *argv)
+char	*remove_enclosed_quotes(char *arg)
 {
 	char	*more_input;
 	char	*result;
 	char	*temp;
-
-	if (argv == NULL)
-		return (argv);
-	result = argv;
-	while (quotes_finder(result, '\"') || quotes_finder(result, '\''))
+	
+	if (arg == NULL)
+		return (arg);
+	
+	result = arg;
+	while ((*arg == '\"' && arg[ft_strlen(arg) - 1] == '\"') \
+		||(*arg == '\'' && arg[ft_strlen(arg) - 1] == '\''))
+	{
+		result = ft_substr(arg, 1, ft_strlen(arg) - 2);
+		free(arg);
+	}
+	while (quotes_finder(&result, '\"') || quotes_finder(&result, '\''))
 	{
 		temp = result;
 		result = ft_strjoin(result, "\n");
@@ -53,8 +83,5 @@ char	*remove_enclosed_quotes(char *argv)
 		
 		free(more_input);
 	}
-	temp = result;
-	result = ft_strtrim(result, "\"\'");
-	free(temp);
 	return (result);
 }
