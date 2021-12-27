6239f058945ab45ef7a0960127776021f2146a99
ahngbeom <bbu0704@gmail.com>
2021-12-27 17:54:33 +0900

    Not Perfect Escape Sequence

--- srcs/utils/escape_sequence.c
+++ srcs/utils/escape_sequence.c
@@ -6,7 +6,7 @@
 /*   By: bahn <bahn@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/27 14:32:31 by bahn              #+#    #+#             */
-/*   Updated: 2021/12/27 17:50:12 by bahn             ###   ########.fr       */
+/*   Updated: 2021/12/27 17:54:19 by bahn             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,22 @@ char	*escape_sequence(char *arg)
 		ptr = arg;
 		while (find)
 		{
-			if (*(find + 1) == '\0')
+			if (*(find + 1) == '\\')
+			{
+				more_input = ft_substr(ptr, 0, find - ptr + 1);
+				if (result == NULL)
+					result = more_input;
+				else
+				{
+					temp = result;
+					result = ft_strjoin(result, more_input);
+					free(temp);
+				}
+				ptr = find += 2;
+				find = ft_strchr(ptr, '\\');
+
+			}
+			else if (*(find + 1) == '\0')
 			{
 				more_input = readline("> ");
 				temp = ft_substr(ptr, 0, ft_strlen(arg) - ft_strlen(find));
@@ -44,7 +59,7 @@ char	*escape_sequence(char *arg)
 				result = ft_strjoin(temp, more_input);
 				free(temp);
 				free(more_input);
-				find = ft_strchr(result + ft_strlen(result) - ft_strlen(more_input), '\\');
+				find = ft_strchr(result, '\\');
 			}
 		}
 		// free(arg);
