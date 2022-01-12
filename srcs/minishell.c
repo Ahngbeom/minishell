30577ef17d751dbeb20247f8105a3a94b284223e
minsikim-42 <minsikim@c1r6s4.42seoul.kr>
2022-01-12 13:10:40 +0900

    > > > > ...

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,7 +6,7 @@
 /*   By: minsikim <minsikim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/18 15:03:56 by bahn              #+#    #+#             */
-/*   Updated: 2022/01/05 12:58:25 by minsikim         ###   ########.fr       */
+/*   Updated: 2022/01/12 13:09:42 by minsikim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,27 +57,54 @@ void	set_flag(t_list *i_list)
 	}
 }
 
+t_list	**dup2_fd(t_list **list, int **fd, int i) // if > > > > ...
+{
+	// t_list		*ptr;
+	t_command	*cont;
+
+	// ptr = *list;
+	while (1)
+	{
+		cont = (*list)->content;
+		if (cont->next_flag == 3) // >
+		{
+			fd[i][1] = open(((t_command *)(*list)->next->content)->argv[0], O_RDWR | O_CREAT | O_TRUNC, 0644);
+			dup2(fd[i][1], STDOUT_FILENO);
+			close(fd[i][1]);
+		}
+		else
+			break ;
+		*list = (*list)->next;
+	}
+	return (list);
+}
+
 void	ft_pipe(t_list	**list)
 {
 	int		**fd;
 	int		i;
 	int		size;
 	t_command	*content;
+	t_command	*exe;
+	t_list		*ptr;
 	pid_t		pid;
 	int			status;
 
 	size = ft_lstsize(g_data.commands);
 	fd = malloc(sizeof(int *) * size);
+	ptr = *list;
+	exe = (*list)->content;
 	i = -1;
 	while (++i < size)
 	{
 		fd[i] = malloc(sizeof(int) * 2);
+		pipe(fd[i]);
 	}
+	exe = (t_command *)(*list)->content;
 	i = -1;
 	while (++i < size && (*list)->content != NULL)
 	{
 		content = (*list)->content;
-		pipe(fd[i]);
 		pid = fork();
 		if (pid == 0) // son
 		{
@@ -89,41 +116,70 @@ void	ft_pipe(t_list	**list)
 			{
 				dup2(fd[i][1], STDOUT_FILENO);
 			}
+			
+			////////////////////////////////////////////////////////////
+			
+			if (content->pre_flag == 3 || content->pre_flag == 4) // (IN) > argv(OUT)
+			{
+				dup2(fd[i - 1][0], STDIN_FILENO);
+			}
 			if (content->next_flag == 3) // argv >
 			{
-				fd[i][1] = open(((t_command *)(*list)->next->content)->argv[0], O_WRONLY | O_CREAT | O_TRUNC, 0644); // S_IROTH : 개인에게 읽기권한 , 0644: 소유자-WR,RD 그룹,개인-RD
-				dup2(fd[i][1], STDOUT_FILENO);
-				if (content->pre_flag == 5) //
-				{
-					dup2(fd[i - 1][0], STDOUT_FILENO);
-					// close(fd[i - 1][0]);
-				}
+				list = dup2_fd(list, fd, i);
+				// fd[i][1] = open(((t_command *)(*list)->next->content)->argv[0], O_RDWR | O_CREAT | O_TRUNC, 0644); // S_IROTH : 개인에게 읽기권한 , 0644: 소유자-WR,RD 그룹,개인-RD
+				// dup2(fd[i][1], STDOUT_FILENO);
 			}
 			if (content->next_flag == 4) // argv >>
 			{
-				fd[i][1] = open(((t_command *)(*list)->next->content)->argv[0], O_WRONLY | O_CREAT | O_APPEND, 0644);
+				fd[i][1] = open(((t_command *)(*list)->next->content)->argv[0], O_RDWR | O_CREAT | O_APPEND, 0644);
 				dup2(fd[i][1], STDOUT_FILENO);
+				close(fd[i][1]);
+			}
+			if (content->pre_flag == 5) // < argv
+			{
+				dup2(fd[i - 1][1], STDOUT_FILENO);
 			}
 			if (content->next_flag == 5) // argv <
 			{
-				fd[i][1] = open(((t_command *)(*list)->next->content)->argv[0], O_RDONLY, 0644);
-				dup2(fd[i][1], STDIN_FILENO);
+				fd[i][0] = open(((t_command *)(*list)->next->content)->argv[0], O_RDONLY, 0644);
+				dup2(fd[i][0], STDIN_FILENO);
+				close(fd[i][0]);
+				if (((t_command *)(*list)->next->content)->next_flag == 3)
+				{
+					printf("why..\n");
+					dup2(fd[i + 1][1], STDIN_FILENO);
+					close(fd[i + 1][1]);
+				}
 			}
-			to_execve_2((*list)->content);
+			if (content->next_flag == 6) //argv <<
+			{
+				fd[i][1] = open("temp", O_RDWR | O_CREAT | O_TRUNC, 0644);
+				dup2(fd[i][1], STDOUT_FILENO);
+			}
+			if (content->pre_flag == 6) // << argv
+			{
+				dup2(fd[i - 1][0], STDIN_FILENO);
+				// to_execve_2((*list - sizeof(*list))->content);
+			}
+			to_execve_2(exe); ///
 		}
 		else // parent
 		{
 			wait(&status);
+			
 			if (content->pre_flag)
 				close(fd[i - 1][0]);
 			if (content->next_flag)
 				close(fd[i][1]);
+			if (content->next_flag == 6) // rm temp
+				unlink("temp");
 			g_data.status = WEXITSTATUS(g_data.status);
 			if (g_data.status == 127)
 				printf("minishell: %s: command not found\n", ((t_command *)(*list)->content)->argv[0]);
 		}
 		if ((*list)->next)
 			*list = (*list)->next;
+		i++;
 	}
 }
 
